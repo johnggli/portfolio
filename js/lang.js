c3e3a0ce28cfabdfed1c356449ae343a02cb636d
John Emerson <john.carv.sousa@gmail.com>
2021-02-16 15:02:13 -0300

    feat: add more translates

--- js/lang.js
+++ js/lang.js
@@ -1,13 +1,19 @@
 var arrLang = {
   'en': {
+    'language': 'English',
     'portfolio': 'Portfolio',
     'about': 'About',
-    'contact': 'Contact'
+    'contact': 'Contact',
+    'portfolioSection1': 'Here are some of my latest work.',
+    'portfolioSection2': 'Click on the images to see details'
   },
   'pt': {
+    'language': 'Português',
     'portfolio': 'Portfólio',
     'about': 'Sobre',
-    'contact': 'Contato'
+    'contact': 'Contato',
+    'portfolioSection1': 'Aqui estão alguns dos meus últimos trabalhos.',
+    'portfolioSection2': 'Clique nas imagens para ver os detalhes'
   }
 };
 
@@ -15,7 +21,7 @@ $(function() {
   $('.translate').click(function() {
     var lang = $(this).attr('id');
 
-    $('.lang').each(function(index, element) {
+    $('.lang').each(function() {
       $(this).text(arrLang[lang][$(this).attr('key')]);
     });
   });
